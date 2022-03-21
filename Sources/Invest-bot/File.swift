c0754033133564fc522ff3bf3df43ec119d69d2b
Lucas Barbosa <lucasbarbosa2807@gmail.com>
2022-03-21 15:36:14 -0300

    ïmplementacao de opcionais

--- Sources/Invest-bot/File.swift
+++ Sources/Invest-bot/File.swift
@@ -21,52 +21,69 @@ struct Result: Codable {
 class DecoderBank{
     
     func app(){
-        let runLoop = CFRunLoopGetCurrent()
-        
-        //        var url = URL(string: "https://yfapi.net/v6/finance/quote")!
-        //
-        //        var querystring = [
-        //            "symbols":"AMD",
-        //            "region":"US",
-        //            "lang":"en"
-        //        ]
-        
-        let headers = [
-            "x-api-key" : "DJcbbxcuNt96LScWh0zuw4Z3DsrrwsG41H0aDOnh"
-        ]
-        
-        var components = URLComponents(string: "https://yfapi.net/v6/finance/quote")!
-        
-        components.queryItems = [
-            URLQueryItem(name: "x-api-key", value: "DJcbbxcuNt96LScWh0zuw4Z3DsrrwsG41H0aDOnh"),
-            URLQueryItem(name: "symbols", value: "AAPL"),
-            URLQueryItem(name: "region", value: "US"),
-            URLQueryItem(name: "lang", value: "en")
-        ]
-        
-        var request = URLRequest(url: components.url!)
-        request.allHTTPHeaderFields = headers
-        request.httpMethod = "GET"
         
         
-        let task = URLSession.shared.dataTask(with: request, completionHandler: { (data,response,error) in
-            if let data = data {
-                do {
-                    let response = try JSONDecoder().decode(FinanceResponse.self, from: data)
-                    print("Name\t\t     Actual Price\t Market Open")
-                    print("\(response.quoteResponse.result[0].shortName)\t \t \(response.quoteResponse.result[0].regularMarketPrice)\t \t \t \(response.quoteResponse.result[0].regularMarketOpen)")
-                    
-                    
-                    CFRunLoopStop(runLoop)
-                }
-                catch {
-                    print(error)
-                }
+
+        print("Welcome")
+        
+        while true {
+            let opt = readLine()
+            switch opt{
+            case "1":
+                let runLoop = CFRunLoopGetCurrent()
+                let headers = [
+                    "x-api-key" : "bIyEkCuO7A4AUpgQ3QCj746X9zpzSUKV6BH3ckn2"
+                ]
+                
+                var components = URLComponents(string: "https://yfapi.net/v6/finance/quote")!
+                
+                components.queryItems = [
+                    URLQueryItem(name: "x-api-key", value: "bIyEkCuO7A4AUpgQ3QCj746X9zpzSUKV6BH3ckn2"),
+                    URLQueryItem(name: "symbols", value: "AAPL,ADBE"),
+                    URLQueryItem(name: "region", value: "US"),
+                    URLQueryItem(name: "lang", value: "en")
+                ]
+                
+                var request = URLRequest(url: components.url!)
+                request.allHTTPHeaderFields = headers
+                request.httpMethod = "GET"
+                
+                
+                let task = URLSession.shared.dataTask(with: request, completionHandler: { (data,response,error) in
+                    if let data = data {
+                        do {
+                            let response = try JSONDecoder().decode(FinanceResponse.self, from: data)
+                            print("Name\t\t  Actual Price\t Market Open")
+                            
+                            for i in 0...1 {
+                                print("\(response.quoteResponse.result[i].shortName) \t  \(response.quoteResponse.result[i].regularMarketPrice)\t \t \(response.quoteResponse.result[i].regularMarketOpen)")
+                                
+                            }
+                            
+//                            print("\(response.quoteResponse.result[0].shortName) \t  \(response.quoteResponse.result[0].regularMarketPrice)\t \t \(response.quoteResponse.result[0].regularMarketOpen)")
+                            
+                            
+                            CFRunLoopStop(runLoop)
+                        }
+                        catch {
+                            print(error)
+                        }
+                    }
+                })
+                task.resume()
+                CFRunLoopRun()
+
+            case "2":
+                print("2")
+            default:
+                break
+        }
+        
+        
+            
+        
             }
-        })
-        task.resume()
-        CFRunLoopRun()
-    }
 }
 
 
+}
