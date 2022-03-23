fe11ea2c722170ca7903788b9895560496809229
Lucas Barbosa <lucasbarbosa2807@gmail.com>
2022-03-23 10:37:46 -0300

    beta

--- Sources/Invest-bot/File.swift
+++ Sources/Invest-bot/File.swift
@@ -3,20 +3,44 @@ import Foundation
 
 struct FinanceResponse: Codable {
     let quoteResponse: QuoteResponse
+    
+    struct QuoteResponse: Codable {
+        let result: [Result]
+    }
+
+    // MARK: - Result
+    struct Result: Codable {
+        let symbol: String
+        let regularMarketPrice: Double
+        let regularMarketOpen: Double
+        let shortName: String
+        
+    }
 }
 
-// MARK: - QuoteResponse
-struct QuoteResponse: Codable {
-    let result: [Result]
+struct RecommendedFinances: Codable{
+    let finance: Finance
+    
+    struct Finance: Codable{
+        let result: Result
+    }
+    
+    
+    struct Result: Codable{
+        let recommendedSymbols: [RecommendedSymbols]
+    }
+    
+    struct RecommendedSymbols: Codable{
+        let symbol: String
+        let score: Double
+        
+    }
+    
 }
 
-// MARK: - Result
-struct Result: Codable {
-    let symbol: String
-    let regularMarketPrice: Double
-    let regularMarketOpen: Double
-    let shortName: String
-}
+
+
+
 
 class DecoderBank{
     
@@ -24,7 +48,15 @@ class DecoderBank{
         
         
 
-        print("Welcome")
+        
+        print("""
+                                Welcome
+            1.Select 1 to visualize investings
+        
+            2.Select 2 to visualize top 5 actual actions based by score
+        
+            3.Press any other button to shotdown the application
+        """)
         
         while true {
             let opt = readLine()
@@ -39,7 +71,7 @@ class DecoderBank{
                 
                 components.queryItems = [
                     URLQueryItem(name: "x-api-key", value: "bIyEkCuO7A4AUpgQ3QCj746X9zpzSUKV6BH3ckn2"),
-                    URLQueryItem(name: "symbols", value: "AAPL,ADBE"),
+                    URLQueryItem(name: "symbols", value: "AAPL,ADBE,ABNB,AMZN,AMD,FB"),
                     URLQueryItem(name: "region", value: "US"),
                     URLQueryItem(name: "lang", value: "en")
                 ]
@@ -53,14 +85,12 @@ class DecoderBank{
                     if let data = data {
                         do {
                             let response = try JSONDecoder().decode(FinanceResponse.self, from: data)
-                            print("Name\t\t  Actual Price\t Market Open")
+                            print("Symbol\t Actual Price\t Market Open\t Name")
                             
-                            for i in 0...1 {
-                                print("\(response.quoteResponse.result[i].shortName) \t  \(response.quoteResponse.result[i].regularMarketPrice)\t \t \(response.quoteResponse.result[i].regularMarketOpen)")
+                            for result in response.quoteResponse.result {
+                                print("\(result.symbol) \t \(result.regularMarketPrice)\t\t\t\(result.regularMarketOpen)\t\t\t \(result.shortName)")
                                 
                             }
-                            
-//                            print("\(response.quoteResponse.result[0].shortName) \t  \(response.quoteResponse.result[0].regularMarketPrice)\t \t \(response.quoteResponse.result[0].regularMarketOpen)")
                             
                             
                             CFRunLoopStop(runLoop)
@@ -72,9 +102,46 @@ class DecoderBank{
                 })
                 task.resume()
                 CFRunLoopRun()
-
+                
             case "2":
-                print("2")
+                let runLoop = CFRunLoopGetCurrent()
+                let headers = [
+                    "x-api-key" : "bIyEkCuO7A4AUpgQ3QCj746X9zpzSUKV6BH3ckn2"
+                ]
+                
+                var components = URLComponents(string:"https://yfapi.net/v6/finance/recommendationsbysymbol/AAPL")!
+                
+                components.queryItems = [
+                    URLQueryItem(name: "x-api-key", value: "bIyEkCuO7A4AUpgQ3QCj746X9zpzSUKV6BH3ckn2"),
+                    URLQueryItem(name: "symbol", value: "AAPL"),
+                ]
+                
+                var request = URLRequest(url: components.url!)
+                request.allHTTPHeaderFields = headers
+                request.httpMethod = "GET"
+                
+                
+                let task = URLSession.shared.dataTask(with: request, completionHandler: { (data,response,error) in
+                    if let data = data {
+                        do {
+                            let response = try JSONDecoder().decode(RecommendedFinances.self, from: data)
+                            print("Symbol\t  Score")
+                            
+                            for result in response.finance.result.recommendedSymbols{
+                                print("\(result.symbol)\t \(result.score)")
+                                
+                            }
+                            
+                            
+                            CFRunLoopStop(runLoop)
+                        }
+                        catch {
+                            print(error)
+                        }
+                    }
+                })
+                task.resume()
+                CFRunLoopRun()
             default:
                 break
         }
